8ed48c7eb788e1bf788082ff685b81abd6981080
JerrelMitchell <jerrelmitchell@ymail.com>
2018-05-12 16:17:12 -0600

    initial commit. add simplecov and update tests to be more explicit with intent. remove true statement from layouts views for application.html.erb.

--- spec/features/companies/user_deletes_a_company_spec.rb
+++ spec/features/companies/user_deletes_a_company_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
-describe "User deletes existing company" do
-  scenario "a user can delete a company" do
-    company = Company.create(name: "ESPN")
+describe 'User deletes existing company' do
+  scenario 'when clicking delete link for a company' do
+    company = Company.create(name: 'ESPN')
     visit companies_path
 
     within(".company_#{company.id}") do
-      click_link "Delete"
+      click_link 'Delete'
     end
 
-    expect(page).to have_content("ESPN was successfully deleted!")
+    expect(page).to have_content('ESPN was successfully deleted!')
+    expect(page).to_not have_link('ESPN')
   end
 end
--- spec/features/companies/user_sees_one_company_spec.rb
+++ spec/features/companies/user_sees_one_company_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
-describe "User sees one company" do
-  scenario "a user sees a company" do
-    company = Company.create!(name: "ESPN")
-    company.jobs.create!(title: "Developer", level_of_interest: 90, city: "Denver")
+describe 'when visiting a companys\' jobs index page' do
+  scenario 'a user sees company name and job title' do
+    company = Company.create!(name: 'ESPN')
+    job = company.jobs.create!(title: 'Developer',
+                               level_of_interest: 90,
+                               city: 'Denver')
 
     visit company_path(company)
 
     expect(current_path).to eq("/companies/#{company.id}/jobs")
-    expect(page).to have_content("ESPN")
-    expect(page).to have_content("Developer")
+    expect(page).to have_content(company.name)
+    expect(page).to have_content(job.title)
   end
 end
