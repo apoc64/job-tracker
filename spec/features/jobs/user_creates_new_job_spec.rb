2b39d7aedbeb5c2ada1bb2723e991f0d301d9dad
JerrelMitchell <jerrelmitchell@ymail.com>
2018-05-12 19:15:18 -0600

    update test with less brittle setup

--- spec/features/jobs/user_creates_new_job_spec.rb
+++ spec/features/jobs/user_creates_new_job_spec.rb
@@ -1,36 +1,45 @@
 require 'rails_helper'
 
-describe "User creates a new job" do
-  scenario "a user can create a new job" do
-    company = Company.create!(name: "ESPN")
+describe 'User creates a new job' do
+  scenario 'after filling out form fields' do
+    name = 'ESPN'
+    title = 'Developer'
+    description = 'So fun!'
+    level_of_interest = 80
+    city = 'Denver'
+    company = Company.create!(name: name)
     visit new_company_job_path(company)
 
-    fill_in "job[title]", with: "Developer"
-    fill_in "job[description]", with: "So fun!"
-    fill_in "job[level_of_interest]", with: 80
-    fill_in "job[city]", with: "Denver"
+    fill_in 'job[title]', with: title
+    fill_in 'job[description]', with: description
+    fill_in 'job[level_of_interest]', with: level_of_interest
+    fill_in 'job[city]', with: city
 
-    click_button "Create"
+    click_button 'Create'
 
-    expect(current_path).to eq("/companies/#{company.id}/jobs/#{Job.last.id}")
-    expect(page).to have_content("ESPN")
-    expect(page).to have_content("Developer")
-    expect(page).to have_content("80")
-    expect(page).to have_content("Denver")
+    job = company.jobs.last
+
+    expect(current_path).to eq(company_job_path(company, job))
+    expect(page).to have_content(name)
+    expect(page).to have_content(title)
+    expect(page).to have_content(description)
+    expect(page).to have_content(level_of_interest)
+    expect(page).to have_content(city)
   end
 
   scenario 'when a user tries to submit an empty job form' do
-    company = Company.create!(name: "ESPN")
+    name = 'ESPN'
+    company = Company.create!(name: name)
     visit new_company_job_path(company)
 
-    fill_in "job[title]", with: ""
-    fill_in "job[description]", with: ""
-    fill_in "job[level_of_interest]", with: 0
-    fill_in "job[city]", with: ""
+    fill_in 'job[title]', with: ''
+    fill_in 'job[description]', with: ''
+    fill_in 'job[level_of_interest]', with: ''
+    fill_in 'job[city]', with: ''
 
-    click_button "Create"
+    click_button 'Create'
 
-    error = "You need to enter information into all form fields before submitting!"
+    error = 'You need to enter information into all form fields before submitting!'
     expect(current_path).to eq(new_company_job_path(company))
     expect(page).to have_content(error)
   end
