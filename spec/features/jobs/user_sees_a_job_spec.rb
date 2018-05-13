5737effa78396e85c0eab28d096e7dcf6d674b5b
JerrelMitchell <jerrelmitchell@ymail.com>
2018-05-12 18:36:04 -0600

    add tests and functionality for when a form is incomplete for creating a new job, add tests and functionality for clicking a job from a company index page

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -7,8 +7,19 @@ describe "User sees a specific job" do
 
     visit company_job_path(company, job)
 
-    expect(page).to have_content("ESPN")
-    expect(page).to have_content("Developer")
-    expect(page).to have_content("70")
+    expect(page).to have_content(job.city)
+    expect(page).to have_content(job.title)
+    expect(page).to have_content(job.level_of_interest)
+  end
+
+  scenario 'user can click link of job to be directed to its show page' do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+
+    visit company_path(company)
+    click_link(job.title)
+
+    expect(current_path).to eq(company_job_path(company, job))
+    expect(page).to have_content("City: #{job.city}")
   end
 end
