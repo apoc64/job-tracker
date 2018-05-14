0320ef87da1d216ae947b749d092f45dfb6b7784
JerrelMitchell <jerrelmitchell@ymail.com>
2018-05-14 17:12:00 -0600

    all tests passing for create functionality for categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,29 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "#{@category.title} added!"
+      redirect_to category_path(@category)
+    else
+      flash[:notice] = 'You must enter a title for your category!'
+      redirect_to new_category_path
+    end
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:title)
+    end
+end
