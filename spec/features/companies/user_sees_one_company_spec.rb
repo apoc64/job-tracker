5b0276b242ba2a2fe0bc844d87d6d7579fae215c
apoc64 <sschwedt@gmail.com>
2018-05-15 12:58:54 -0600

    company show page can link to company edit, removed obselete job link test

--- spec/features/companies/user_sees_one_company_spec.rb
+++ spec/features/companies/user_sees_one_company_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
-describe 'when visiting a companys\' jobs index page' do
-  scenario 'a user sees company name and job title' do
-    company           = Company.create!(name: 'ESPN')
+describe 'when visiting a company show page' do
+  scenario 'a user sees company name' do
+    company = Company.create!(name: 'ESPN')
 
     visit company_path(company)
 
     expect(page).to have_content(company.name)
+  end
+
+  scenario 'a user can edit the company' do
+    company = Company.create!(name: 'ESPN')
+
+    visit company_path(company)
+
+    click_link "Edit"
+
+    expect(current_path).to eq(edit_company_path(company))
   end
 end
