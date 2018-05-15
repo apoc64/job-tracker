b6c38fddd6bee77676bbbfbc1dea96ec90c32d08
JerrelMitchell <jerrelmitchell@ymail.com>
2018-05-15 17:25:40 -0600

    change everything to do with job comments to only call them comments

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resources :jobs
   end
 
-  resources :job_comments, only: :create
+  resources :comments, only: :create
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/models/job_comment_spec.rb
+++ spec/models/job_comment_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe JobComment, type: :model do
+describe Comment, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:content) }
   end
