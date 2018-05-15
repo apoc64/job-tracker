3a8be01063e0d786240229ff2acbac6d0cb02ea3
apoc64 <sschwedt@gmail.com>
2018-05-15 12:04:10 -0600

    shows category with edit and delete for jobs

--- spec/features/categories/user_sees_one_category_spec.rb
+++ spec/features/categories/user_sees_one_category_spec.rb
@@ -33,4 +33,48 @@ describe 'User visits single category page' do
     expect(page).to have_content(job2.city)
     expect(page).to have_content(job2.title)
   end
+  scenario 'it can delete a job' do
+    category = Category.create(title: 'Developer')
+    company = Company.create!(name: 'ESPN')
+    job1 = category.jobs.create!(title: 'Sr Dev',
+                               level_of_interest: 30,
+                               city: 'Denver',
+                               company: company)
+    job2 = category.jobs.create!(title: 'Jr Dev',
+                               level_of_interest: 40,
+                               city: 'New York',
+                               company: company)
+
+    visit category_path(category)
+
+    within(".job_#{job1.id}") do
+      click_link 'Delete'
+    end
+
+    within("main") do
+      expect(page).to_not have_content(job1.title)
+    end
+  end
+
+  scenario 'it can go to edit job page' do
+    category = Category.create(title: 'Developer')
+    company = Company.create!(name: 'ESPN')
+    job1 = category.jobs.create!(title: 'Sr Dev',
+      level_of_interest: 30,
+      city: 'Denver',
+      company: company)
+    job2 = category.jobs.create!(title: 'Jr Dev',
+        level_of_interest: 40,
+        city: 'New York',
+        company: company)
+
+    visit category_path(category)
+
+    within(".job_#{job1.id}") do
+      click_link 'Edit'
+    end
+
+    expect(current_path).to eq(edit_company_job_path(job1.company, job1))
+    expect(page).to_not have_content(job1.title)
+  end
 end
