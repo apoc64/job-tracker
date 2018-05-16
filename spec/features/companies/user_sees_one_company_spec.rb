8e9a6aba8896b08b702eb443d37fe1e336208665
apoc64 <sschwedt@gmail.com>
2018-05-15 18:16:42 -0600

    user can delete company from the show page

--- spec/features/companies/user_sees_one_company_spec.rb
+++ spec/features/companies/user_sees_one_company_spec.rb
@@ -18,4 +18,19 @@ describe 'when visiting a company show page' do
 
     expect(current_path).to eq(edit_company_path(company))
   end
+
+  scenario 'a user can delete the company' do
+    company = Company.create!(name: 'ESPN')
+
+    visit company_path(company)
+
+    click_link "Delete"
+
+    expect(current_path).to eq(companies_path)
+
+    expect(page).to_not have_link(company.name)
+    expect(page).to have_content('ESPN was successfully deleted!')
+  end
+
+
 end
