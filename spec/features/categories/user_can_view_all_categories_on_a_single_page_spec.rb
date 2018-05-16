c474fd7b134ba299ffb7cbcfb07ded67e8972ed5
JerrelMitchell <jerrelmitchell@ymail.com>
2018-05-16 10:57:38 -0600

    update show page to be more inline with wire frame

--- spec/features/categories/user_can_view_all_categories_on_a_single_page_spec.rb
+++ spec/features/categories/user_can_view_all_categories_on_a_single_page_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'User visits all categories page' do
+describe 'User visits /categories' do
   scenario 'a user can see all categories' do
     category1 = Category.create(title: 'Developer')
     category2 = Category.create(title: 'Sewer Scrubber')
@@ -11,7 +11,7 @@ describe 'User visits all categories page' do
     expect(page).to have_content(category2.title)
   end
 
-  scenario 'a user delete from that page' do
+  scenario 'a user can delete a category from index' do
     category = Category.create(title: 'Sewer Scrubber')
 
     visit categories_path
@@ -25,7 +25,7 @@ describe 'User visits all categories page' do
     expect(page).to_not have_link(category.title)
   end
 
-  scenario 'a user edits from that page' do
+  scenario 'can click an edit link to be sent to the category/id/edit page' do
     category = Category.create(title: 'Sewer Scrubber')
 
     visit categories_path
@@ -37,6 +37,17 @@ describe 'User visits all categories page' do
     expect(current_path).to eq(edit_category_path(category))
   end
 
+  scenario 'can click a link showing number of jobs for category and be directed to show page for category' do
+    category = Category.create(title: 'Sewer Scrubber')
+
+    visit categories_path
+
+    within(".category_#{category.id}") do
+      click_link '0 jobs'
+    end
+
+    expect(current_path).to eq(category_path(category))
+  end
 
 
 end
